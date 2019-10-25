28aee3c56f31c27dae6d43879d10890b85c24efb
Thib-Thib <48785856+Thib-Thib@users.noreply.github.com>
2019-10-25 07:51:03 +0200

    Add files via upload

---
+++ s2/s2_activite_script_R.R
@@ -0,0 +1,22 @@
+#################################################################################
+#                             mooc - imepp - s2                                 #
+# analyse de l'impact d'icitations monétaires sur l'obtentions de résultats HIV #
+#################################################################################
+
+# partie 1 : statistiques descriptives
+# 1. présentation globale
+# 2. présentation séparée
+
+# partie 2 : analyse graphique
+# 1. histogramme
+# 2. histogramme
+
+# partie 3 : analyse MCO
+# 1. première estimation
+# 2. seconde estimation
+# 3. troisième estimation
+# 4. résumé
+
+# partie 4 : hétérogénéité de l'impact
+# 1. différence d'effet
+# 2. différence d'effet
